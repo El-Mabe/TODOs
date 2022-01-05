033db6862fd577d151d579f715e69077fac890ae
Julian David Mabesoy Jimenez <julian.mabesoy@correo.icesi.edu.co>
2022-01-04 22:47:02 -0500

    first commit

---
+++ src/App/index.js
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import { AppUI } from './AppUI';
+
+
+/**
+ * Creación de Custom Kook para Local Storage
+ * @param {*} itemName 
+ * @param {*} initialValue 
+ * @returns un arreglo con los items que hay en el localStorage y otro que permite editar los items
+ */
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+  
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [
+    item,
+    saveItem,
+  ];
+}
+
+
+function App() {
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const [searchValue, setSearchValue] = useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  //si no hay ningun caracter en el Search muestra todos las tareas 
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } 
+  //Filtra la lista de tareas dependiendo los caracteres que se ingresen a la hora de buscar
+  else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+  return (
+    <AppUI
+    totalTodos={totalTodos}
+    completedTodos={completedTodos}
+    searchValue={searchValue}
+    setSearchValue={setSearchValue}
+    searchedTodos={searchedTodos}
+    completeTodo={completeTodo}
+    deleteTodo={deleteTodo}
+  />
+  );
+};
+
+export default App;
